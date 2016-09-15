d591f2c58bdea0248899f7c1e42445b225e39c03
Andrew Harvey <andrew@mootpointer.com>
2016-09-15 14:36:06 +1000

    remove cruft from mootpointer

--- modules/people/manifests/mootpointer.pp
+++ modules/people/manifests/mootpointer.pp
@@ -1,21 +1,9 @@
 class people::mootpointer {
-  include projects::zova_api
-
-  class neovim {
-    homebrew::tap { 'neovim/neovim': }
-    package {
-      'neovim' : install_options => ['--HEAD'];
-    }
-  }
-  include neovim
   homebrew::tap { 'caskroom/versions':  } ->
   package {
     'elixir': ;
     '1password': provider => 'brewcask' ;
-    'flash': provider => 'brewcask';
-    'iterm2-beta': provider => 'brewcask';
     'dropbox': provider => 'brewcask';
-    'mailbox': provider => 'brewcask';
     'viscosity' : provider => 'brewcask';
     'heroku-toolbelt' : ensure => 'latest';
     'sonos' : provider => 'brewcask';
