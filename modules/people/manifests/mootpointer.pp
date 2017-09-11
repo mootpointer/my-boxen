eb95b29a72af862dc261f805a1a6558c3b503d77
Andrew Harvey <andrew@mootpointer.com>
2017-09-11 16:43:12 +1000

    More things

--- modules/people/manifests/mootpointer.pp
+++ modules/people/manifests/mootpointer.pp
@@ -1,5 +1,7 @@
 class people::mootpointer {
-  homebrew::tap { 'caskroom/versions':  } ->
+  
+  homebrew::tap {'railwaycat/emacsmacport': } ->
+
   package {
     'elixir': ;
     '1password': provider => 'brewcask' ;
@@ -10,6 +12,48 @@ class people::mootpointer {
     'the_silver_searcher': ;
     'p4merge': provider => 'brewcask';
     'exercism':;
+    'fantastical': provider => 'brewcask';
+    'iterm2': provider => 'brewcask';
+    'emacs-mac': ;
   }
 
+  $home     = "/Users/${::boxen_user}"
+
+  file { 'homeshickroot':
+    path => "${home}/.homesick",
+    ensure => directory
+  } 
+
+  file { 'homeshickdir':
+    path => "${home}/.homesick/repos",
+    ensure => directory,
+  }
+
+  repository { 'homeshick':
+    source => 'andsens/homeshick',
+    path   => "${home}/.homesick/repos/homeshick",
+    require => File['homeshickdir']
+  }
+  repository { 'my-dotfiles':
+    ensure  => 'origin/HEAD', # https://github.com/boxen/puppet-repository/issues/12
+    source  => 'mootpointer/dotfiles', # Change me
+    path    => "${home}/.homesick/repos/dotfiles",
+    require => File['homeshickdir'],
+    notify  => Exec['link-dotfiles']
+  }
+
+  exec { 'link-dotfiles':
+    command     => "bash -c 'source ${home}/.homesick/repos/homeshick/homeshick.sh; homeshick link dotfiles --force'",
+    #refreshonly => true
+  }
+
+  repository { "${home}/.emacs.d":
+    source => 'syl20bnr/spacemacs',
+    extra  => [
+      '--recursive',
+    ],
+  }
+
+  include osx::global::enable_keyboard_control_access
+  include osx::no_network_dsstores
 }
